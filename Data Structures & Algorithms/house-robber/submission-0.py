96fcb20fb90230fa0733f163f8c8ea54cce86cf1
santisky8 <113253355+santisky8@users.noreply.github.com>
2026-08-30 17:25:27 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            current_max = max(n + rob1, rob2)
+
+            rob1 = rob2
+
+            rob2 = current_max
+
+        return rob2
